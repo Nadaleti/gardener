7f448d8f3ea54950362c13b27ecd93258685016b
Nadaleti <mat.nadaleti@hotmail.com>
2020-06-23 17:24:20 -0300

    Adding authentication middleware

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,5 +1,5 @@
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
+import dotenv from 'dotenv';
+import fs from 'fs';
 import path from 'path';
 
 const env = process.env.NODE_ENV || 'dev';
