7b49eebeb1f6576d397af52951993173df33a10d
Nadaleti <mat.nadaleti@hotmail.com>
2020-06-24 15:17:07 -0300

    Adding error handler on server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
 import cors from 'cors';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { createConnection } from "typeorm";
+
+import APIError from "./errors/APIError";
 
 const app = express();
 
@@ -13,5 +15,18 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json('Hello world!');
 })
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const error = new APIError('Not found', 404);
+
+  next(error);
+});
+
+app.use((error: APIError, req: Request, res: Response, next: NextFunction) => {
+  res.status(error.status || 500).send({
+    status: error.status || 500,
+    message: error.message || 'Internal Server Error',
+  })
+});
 
 app.listen(3333);
