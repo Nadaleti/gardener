f27a278e312ba1bc341767df026cb6f4df4ea42c
Nadaleti <mat.nadaleti@hotmail.com>
2020-07-08 15:27:28 -0300

    Web project setup

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>Some text to start an application</p>
+    </div>
+  );
+}
+
+export default App;
